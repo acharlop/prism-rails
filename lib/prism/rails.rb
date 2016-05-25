2c148f3722a5c19850ed1d1ed2f8cded34d56389
Avi Charlop <avicharlop@gmail.com>
2016-05-25 13:32:32 -0400

    initial commit

---
+++ lib/prism/rails.rb
@@ -0,0 +1,7 @@
+require "prism/rails/version"
+
+module Prism
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/prism/rails/version.rb
@@ -0,0 +1,5 @@
+module Prism
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
